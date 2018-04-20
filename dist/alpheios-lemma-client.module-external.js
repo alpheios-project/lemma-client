4c1051ebdb641e388bb335412dfb498652bd7c9e
irina060981 <irina060981@yandex.ru>
2018-04-20 19:04:10 +1000

    transforming to use as Promise in uicontroller

--- dist/alpheios-lemma-client.module-external.js
+++ dist/alpheios-lemma-client.module-external.js
@@ -841,7 +841,16 @@ class AlpheiosLemmaTranslationsAdapter extends BaseLemmaTranslationsAdapter {
 }
 
 class LemmaTranslations {
-  static async fetchTranslations (lemmaList, outLang) {
+  /**
+   * Send requests to either short of full definitions depending on the `lookupFunction` value.
+   * @param {Lemma} lemma - A lemma we need definitions for.
+   * @param {Object} requestOptions - With what options run a request.
+   * @param {String} lookupFunction - A name of an adapter lookup function to use for a request.
+   * @return {Promise[]} Array of Promises, one for each request. They will be either fulfilled with
+   * a Definition object or resolved with an error if request cannot be made/failed/timeout expired.
+   */
+
+  static async fetchTranslationsBackup (lemmaList, outLang) {
     // let languageCode = LanguageModelFactory.getLanguageCodeFromId(languageID)
 
     console.log('********starting fetching translations');
@@ -855,6 +864,28 @@ class LemmaTranslations {
       Translation.loadTranslations(lemma, curTranslations);
     }
     console.log('********finish fetching translations', translationsList);
+  }
+
+  static fetchTranslations (lemmaList, outLang) {
+    return new Promise$1((resolve, reject) => {
+      try {
+        let lemmaAdapter = new AlpheiosLemmaTranslationsAdapter();
+        lemmaAdapter.getTranslationsList(lemmaList, outLang)
+          .then(function (translationsList) {
+            for (let lemma of lemmaList) {
+              let curTranslations = translationsList.find(function (element) { return element.in === lemma.word });
+              Translation.loadTranslations(lemma, curTranslations);
+            }
+            console.log('********finish fetching translations', translationsList);
+            resolve(translationsList);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      } catch (error) {
+        reject(error);
+      }
+    })
   }
 }
 
--- src/lemma_translations.js
+++ src/lemma_translations.js
@@ -1,8 +1,18 @@
 import { Translation } from 'alpheios-data-models'
 import AlpheiosLemmaTranslationsAdapter from './alpheios/alpheios_adapter'
+import Promise from 'promise-polyfill'
 
 export default class LemmaTranslations {
-  static async fetchTranslations (lemmaList, outLang) {
+  /**
+   * Send requests to either short of full definitions depending on the `lookupFunction` value.
+   * @param {Lemma} lemma - A lemma we need definitions for.
+   * @param {Object} requestOptions - With what options run a request.
+   * @param {String} lookupFunction - A name of an adapter lookup function to use for a request.
+   * @return {Promise[]} Array of Promises, one for each request. They will be either fulfilled with
+   * a Definition object or resolved with an error if request cannot be made/failed/timeout expired.
+   */
+
+  static async fetchTranslationsBackup (lemmaList, outLang) {
     // let languageCode = LanguageModelFactory.getLanguageCodeFromId(languageID)
 
     console.log('********starting fetching translations')
@@ -16,5 +26,27 @@ export default class LemmaTranslations {
       Translation.loadTranslations(lemma, curTranslations)
     }
     console.log('********finish fetching translations', translationsList)
+  }
+
+  static fetchTranslations (lemmaList, outLang) {
+    return new Promise((resolve, reject) => {
+      try {
+        let lemmaAdapter = new AlpheiosLemmaTranslationsAdapter()
+        lemmaAdapter.getTranslationsList(lemmaList, outLang)
+          .then(function (translationsList) {
+            for (let lemma of lemmaList) {
+              let curTranslations = translationsList.find(function (element) { return element.in === lemma.word })
+              Translation.loadTranslations(lemma, curTranslations)
+            }
+            console.log('********finish fetching translations', translationsList)
+            resolve(translationsList)
+          })
+          .catch(error => {
+            reject(error)
+          })
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 }
